30380c04ddca065beab954511627ec4fb1a47749
Ajiprie <99744907+Ajiprie@users.noreply.github.com>
2024-10-03 00:58:47 +0700

    Remove run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
 
-# Install required packages
-sudo apt update
-sudo apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-# Start the miner in a detached screen session
-screen -d -m ./hellminer -c stratum+tcp://na.luckpool.net:3960 -u "RP3HXZoebHyGBL6nBhZe5f4jDfE1RMGfn1.$(shuf -i 1-100000 -n 1)" -p x --cpu 4
