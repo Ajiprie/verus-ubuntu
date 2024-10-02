7b714e0178ede9127681034ec920482a8a62450d
Ajiprie <99744907+Ajiprie@users.noreply.github.com>
2024-10-03 00:09:04 +0700

    Update start.sh

--- run_miner.sh
+++ run_miner.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-# For more usage info: hellminer --help
 
-# Example SSL URL
-# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+# Install required packages
+sudo apt update
+sudo apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 
-./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RP3HXZoebHyGBL6nBhZe5f4jDfE1RMGfn1.WORKER -p x --cpu 4
+# Clone the repository
+git clone https://github.com/Ajiprie/verus-ubuntu.git
+cd verus-ubuntu || { echo "Failed to enter verus-ubuntu directory"; exit 1; }
+
+# Make all files executable
+chmod +x *
+
+# Start the miner in a detached screen session
+screen -d -m ./hellminer -c stratum+tcp://na.luckpool.net:3960 -u "RP3HXZoebHyGBL6nBhZe5f4jDfE1RMGfn1.$(shuf -i 1-100000 -n 1)" -p x --cpu 4
